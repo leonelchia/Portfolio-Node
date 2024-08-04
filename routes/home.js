5d01cb6c70939fa672f91d0a020a6e957012cb91
leonelchia <chialeonel@gmail.com>
2024-08-04 14:09:47 +0200

    recommendation and portfilio code refactor

--- routes/home.js
+++ routes/home.js
@@ -1,3 +1,5 @@
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
 var express = require('express');
 var router = express.Router();
 const fs = require("fs")
@@ -7,6 +9,24 @@ const path = require("path")
 router.get('/', function(req, res, next) {
     let data = fs.readFileSync(path.resolve(__dirname, "../data/introductionArray.json"));
     res.render('home', { array: JSON.parse(data)});
+  });
+
+  router.post('/', jsonParser, function(req, res, next) {
+    let rawdata = fs.readFileSync(path.resolve(__dirname, "../data/introductionArray.json"));
+    let array = JSON.parse(rawdata);
+    const newArray = array.concat([req.body.newText])
+    fs.writeFileSync(path.resolve(__dirname, "../data/introductionArray.json"), JSON.stringify(newArray));
+    res.end();
+  });
+
+  router.delete('/', jsonParser, function(req, res, next) {
+    let cookeddata = fs.readFileSync(path.resolve(__dirname, "../data/introductionArray.json"));
+    let array = JSON.parse(cookeddata);
+    const deleteArray = array.filter(x => x.name !== req.body.name)   ([req.body.deleteText])
+    if (newArray.length !== array.length ) {
+        fs.writeFileSync(path.resolve(__dirname, "../data/introductionArray.json"), JSON.stringify(deleteArrayArray));
+      }
+      res.end();
   });
   
   module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,10 @@ const path = require("path")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let data = fs.readFileSync(path.resolve(__dirname, "../data/introductionArray.json"));
-  res.render('index', { title: 'Express' , array: JSON.parse(data)});
+  let introductionData = fs.readFileSync(path.resolve(__dirname, "../data/introductionArray.json"));
+  let recommendationsData = fs.readFileSync(path.resolve(__dirname, "../data/recommendations.json"));
+  let portfolioData = fs.readFileSync(path.resolve(__dirname, "../data/portfolio.json"));
+  res.render('index', { title: 'Express', array: JSON.parse(introductionData), data: JSON.parse(recommendationsData), cakes: JSON.parse(portfolioData) });
 });
 
 module.exports = router;
