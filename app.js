e124375c09c78a62ae99dc037b5d96cf9eb4c995
leonelchia <chialeonel@gmail.com>
2024-08-09 19:21:24 +0200

    add login

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport')
+var session = require('express-session');
+var JsonStore = require('express-session-json')(session);
 
 var indexRouter = require('./routes/index');
 var homeRouter = require('./routes/home');
@@ -11,6 +14,7 @@ var servicesRouter = require('./routes/services');
 var recommendationsRouter = require('./routes/recommendations');
 var portfolioRouter = require('./routes/portfolio');
 var contactRouter = require('./routes/contact');
+var loginRouter = require('./routes/login');
 
 var app = express();
 
@@ -27,6 +31,13 @@ app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
 app.use(express.static(__dirname + '/node_modules/jquery/dist/'));
 app.use(express.static(__dirname + '/node_modules/typed.js/lib/'));
 app.use(express.static(__dirname + '/node_modules/bootstrap-icons'));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  store: new JsonStore()
+}));
+app.use(passport.authenticate('session'));
 app.use('/', indexRouter);
 app.use('/home', homeRouter);
 app.use('/about', aboutRouter);
@@ -34,6 +45,7 @@ app.use('/services', servicesRouter);
 app.use('/recommendations', recommendationsRouter);
 app.use('/portfolio', portfolioRouter);
 app.use('/contact', contactRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -50,5 +62,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
